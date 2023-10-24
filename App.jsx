795df5dd0f22ca3d36c1ff4ee42db20fdcb15aa0
tanish2main <142720889+tanish2main@users.noreply.github.com>
2023-10-25 00:20:08 +0530

    React TODO app

---
+++ App.jsx
@@ -0,0 +1,73 @@
+
+import { response } from "express";
+import React, { useEffect } from "react";
+
+
+function useTodos() {
+  const [todos, setTodos] = React.useState([]);
+
+
+  React.useEffect(()=> {
+    fetch("http://localhost:3000/todos", {
+      method: "GET"
+    }).then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+        setTodos(data);
+      })
+    });
+
+    setInterval(() => {
+      fetch("http://localhost:3000/todos", {
+        method: "GET"
+      }).then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+          setTodos(data);
+        })
+      });
+    },1000)
+  },[])
+
+  return todos;
+}
+
+function App() {
+  const [todos, setTodos] = React.useState (
+    []
+  );
+ 
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/todos", {
+      method: "GET"
+    }).then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+        setTodos(data);
+      })
+
+    });
+   
+
+  }, []);
+
+  
+  
+
+  return (
+    <div>
+      {todos.map(todo => {
+        return <div>
+          {todo.title}
+          {todo.description}
+          <button>Delete</button>
+          <br/>
+        </div>
+      })}
+    </div>
+  )
+
+
+}
+export default App;
