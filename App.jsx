d301a4568af17b8de1062064182acfb420b7f77f
helping_wizard <121712960+helpingwizard@users.noreply.github.com>
2023-10-25 00:29:52 +0530

    Optimised and cleaner React code

--- App.jsx
+++ App.jsx
@@ -1,73 +1,51 @@
-
-import { response } from "express";
-import React, { useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
 
 function useTodos() {
-  const [todos, setTodos] = React.useState([]);
+  const [todos, setTodos] = useState([]);
 
-
-  React.useEffect(()=> {
-    fetch("http://localhost:3000/todos", {
-      method: "GET"
-    }).then((response) => {
-      response.json().then((data) => {
-        console.log(data);
-        setTodos(data);
-      })
-    });
-
-    setInterval(() => {
-      fetch("http://localhost:3000/todos", {
-        method: "GET"
-      }).then((response) => {
-        response.json().then((data) => {
-          console.log(data);
+  useEffect(() => {
+    // Fetch the todos when the component mounts
+    const fetchTodos = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/todos", {
+          method: "GET"
+        });
+        if (response.ok) {
+          const data = await response.json();
           setTodos(data);
-        })
-      });
-    },1000)
-  },[])
+        }
+      } catch (error) {
+        console.error("Error fetching todos:", error);
+      }
+    };
+
+    fetchTodos(); // Call the function to fetch todos when the component mounts
+
+    // Use setInterval to periodically fetch todos
+    const interval = setInterval(fetchTodos, 1000);
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
 
   return todos;
 }
 
 function App() {
-  const [todos, setTodos] = React.useState (
-    []
-  );
- 
-
-  React.useEffect(() => {
-    fetch("http://localhost:3000/todos", {
-      method: "GET"
-    }).then((response) => {
-      response.json().then((data) => {
-        console.log(data);
-        setTodos(data);
-      })
-
-    });
-   
-
-  }, []);
-
-  
-  
+  const todos = useTodos();
 
   return (
     <div>
-      {todos.map(todo => {
-        return <div>
-          {todo.title}
-          {todo.description}
-          <button>Delete</button>
-          <br/>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <p>{todo.title}</p>
+          <p>{todo.description}</p>
+          <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          <br />
         </div>
-      })}
+      ))}
     </div>
-  )
-
-
+  );
 }
+
 export default App;
